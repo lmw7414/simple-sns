19c8cde516e4b473f1565241bec7b4fe5896f4aa
Lee Min Woo <lmw7414@naver.com>
2023-09-13 00:45:05 +0900

    #1 - 회원가입및 로그인에 대한 테스트 작성 및 구현
    회원가입, 로그인 시의 모든 상황에 대해 테스트를 작성하였고, UserService에 회원가입, 로그인 관련 메서드를 구현하였다.

---
+++ src/main/java/com/fastcampus/simplesns/service/UserService.java
@@ -0,0 +1,41 @@
+package com.fastcampus.simplesns.service;
+
+import com.fastcampus.simplesns.exception.ErrorCode;
+import com.fastcampus.simplesns.exception.SnsApplicationException;
+import com.fastcampus.simplesns.model.User;
+import com.fastcampus.simplesns.model.entity.UserEntity;
+import com.fastcampus.simplesns.repository.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserEntityRepository userEntityRepository;
+
+    @Transactional
+    public User join(String userName, String password) {
+        // 회원가입하려는 userName으로 회원가입된 user가 있는지
+        userEntityRepository.findByUserName(userName).ifPresent(it -> {
+            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", userName));
+        });
+
+        // 회원가입 진행  -> user를 등록
+        UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, password));
+        return User.fromEntity(userEntity);
+    }
+
+    //TODO: implement
+    public String login(String userName, String password) {
+        // 회원가입 여부 체크
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, ""));
+        // 비밀번호 체크
+        if (!userEntity.getPassword().equals(password)) {
+            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, "");
+        }
+        // 토큰 생성
+        return "";
+    }
+}
