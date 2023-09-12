5436e6e4ea6fdc9553c583479ff13253d7e7b8cd
Lee Min Woo <lmw7414@naver.com>
2023-09-13 00:33:00 +0900

    #1 - 회원가입 시 발생할 수 있는 예외상황에 대한 예외 클래스 정의
    사용자가 회원가입을 시도할 때 발생할 수 있는 예외상황에 대해 클래스로 정의하였다. 
    
    우선 중복된 이름으로 회원가입한 유저가 있는 경우의 예외상황을 `ErrorCode`에 명시해두었고, 그 밖의 예외 상황에 대해서는 임시로 `INTERNAL_SERVER_ERROR`로 명시해두었다.

---
+++ src/main/java/com/fastcampus/simplesns/exception/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.fastcampus.simplesns.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+    private HttpStatus status;
+    private String message;
+}
