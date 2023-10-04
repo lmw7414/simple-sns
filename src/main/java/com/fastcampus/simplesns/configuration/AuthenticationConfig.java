2f1c824c976caba1eb3774b162b89d3817da0cc3
Lee Min Woo <lmw7414@naver.com>
2023-10-05 03:38:30 +0900

    #5 - 토큰 검증을 위한 필터 및 관련 메서드 기능 구현
    해당 서비스에서는 유저 인증 방식에 JWT를 사용.
    따라서 토큰이 유효한지에 대한 검증 필터를 추가하고 토큰 검증 관련 메서드들을 구현함.
    
    ## JwtTokenUtils
    - 사용자에 대한 프로퍼티 추출 메서드
    - 토큰의 유효기간이 지났는지 확인하는 메서드
    - 유저의 이름 정보 가져오기
    
    ## UserService의 loadUserByUsername()
    - 해당 메서드는 UserDetailsService에 있는 메서드지만 커스텀하게 구현하기 위해 상속받지 않았음.

--- src/main/java/com/fastcampus/simplesns/configuration/AuthenticationConfig.java
+++ src/main/java/com/fastcampus/simplesns/configuration/AuthenticationConfig.java
@@ -1,15 +1,25 @@
 package com.fastcampus.simplesns.configuration;
 
+import com.fastcampus.simplesns.configuration.filter.JwtTokenFilter;
+import com.fastcampus.simplesns.exception.CustomAuthenticationEntryPoint;
+import com.fastcampus.simplesns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserService userService;
+    @Value("${jwt.secret-key}")
+    private String key;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -19,6 +29,8 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
                 ;
                 //TODO: Security 핸들링을 하다 Exception이 던져졌을 경우 특정 엔트리 포인트로 가도록 하자
                 //.exceptionHandling()
