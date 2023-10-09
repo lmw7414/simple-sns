e69246e49966782eef7cee40131b439d65ec0eb9
Lee Min Woo <lmw7414@naver.com>
2023-10-09 22:52:20 +0900

    #5 - 포스트 조회기능 테스트 코드 작성 및 구현
    전체 포스트 조회, 내가 작성한 포스트만 조회 가능하도록 기능을 설계하고 구현하였다.

--- src/main/java/com/fastcampus/simplesns/controller/PostController.java
+++ src/main/java/com/fastcampus/simplesns/controller/PostController.java
@@ -7,8 +7,11 @@ import com.fastcampus.simplesns.controller.response.Response;
 import com.fastcampus.simplesns.model.Post;
 import com.fastcampus.simplesns.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -33,5 +36,15 @@ public class PostController {
     public Response<Void> delete(@PathVariable Integer postId, Authentication authentication) {
         postService.delete(postId, authentication.getName());
         return Response.success();
+    }
+
+    @GetMapping
+    public Response<Page<PostResponse>> list(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.list(pageable).map(PostResponse::fromPost));
+    }
+
+    @GetMapping("/my")
+    public Response<Page<PostResponse>> my(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.my(authentication.getName(), pageable).map(PostResponse::fromPost));
     }
 }
