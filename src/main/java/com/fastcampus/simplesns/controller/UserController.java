e842a61ad2b9f63e14cde15958b3954e3a62b8e5
Lee Min Woo <lmw7414@naver.com>
2024-01-14 02:05:26 +0900

    #16 - 알람 서비스 코드 작성

--- src/main/java/com/fastcampus/simplesns/controller/UserController.java
+++ src/main/java/com/fastcampus/simplesns/controller/UserController.java
@@ -9,6 +9,7 @@ import com.fastcampus.simplesns.controller.response.UserLoginResponse;
 import com.fastcampus.simplesns.exception.ErrorCode;
 import com.fastcampus.simplesns.exception.SnsApplicationException;
 import com.fastcampus.simplesns.model.User;
+import com.fastcampus.simplesns.service.AlarmService;
 import com.fastcampus.simplesns.service.UserService;
 import com.fastcampus.simplesns.util.ClassUtils;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -23,8 +25,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AlarmService alarmService;
 
-    // TODO: implement
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
         User user = userService.join(request.getName(), request.getPassword());
@@ -43,5 +45,13 @@ public class UserController {
                 () -> new SnsApplicationException(ErrorCode.INTERNAL_SERVER_ERROR,
                         "Casing to User class failed"));
         return Response.success(userService.alarmList(user.getId(), pageable).map(AlarmResponse::fromAlarm));
+    }
+
+    @GetMapping("/alarm/subscribe")
+    public SseEmitter subscribe(Authentication authentication) {
+        User user = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), User.class).orElseThrow(
+                () -> new SnsApplicationException(ErrorCode.INTERNAL_SERVER_ERROR,
+                        "Casing to User class failed"));
+        return alarmService.connectAlarm(user.getId());
     }
 }
